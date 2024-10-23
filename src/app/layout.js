de08b4acc8ff2e21916f73aa06cb2bbf12291799
Anuj3553 <anujverma3553@gmail.com>
2024-10-23 14:02:53 +0530

    Corrected the  Metatags

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
-import Script from 'next/script'
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,39 +15,49 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Images */}
-        <link rel="icon" href="Favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="Favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="Favicon/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="Favicon/android-chrome-512x512.png" />
         {/* Meta Tags */}
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Join Footloosemonkey, the ultimate talent competition for kids aged 6-12! Showcase your skills in dancing, singing, acting, and more in a fun, encouraging environment. Discover and celebrate young talents today!" />
-        <meta name="keywords" content="Footloosemonkey" />
-        <meta name="author" content="..." />
+        <meta name="keywords" content="Footloosemonkey, talent competition, kids talent, singing competition, dancing competition" />
+        <meta name="author" content="A&W Technologies" />
+        
+        {/* Open Graph Tags for Social Sharing */}
         <meta property="og:title" content="Footloosemonkey - Where Young Talents Shine" />
         <meta property="og:description" content="Join Footloosemonkey, the ultimate talent competition for kids aged 6-12! Showcase your skills in dancing, singing, acting, and more in a fun, encouraging environment. Discover and celebrate young talents today!" />
         <meta property="og:image" content="/img/og-image.png" />
         <meta property="og:url" content="https://www.footloosemonkey.club" />
+        
+        {/* Twitter Cards */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Footloosemonkey - Where Young Talents Shine" />
         <meta name="twitter:description" content="Join Footloosemonkey, the ultimate talent competition for kids aged 6-12! Showcase your skills in dancing, singing, acting, and more in a fun, encouraging environment. Discover and celebrate young talents today!" />
         <meta name="twitter:image" content="/img/twitter-image.png" />
+
+        {/* Favicon and Icons */}
+        <link rel="icon" href="Favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="Favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="Favicon/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="Favicon/android-chrome-512x512.png" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+
+        {/* Page Title */}
         <title>Footloosemonkey</title>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8590034113156778"
-          crossorigin="anonymous"></script>
+
+        {/* Google Ads Script */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8590034113156778" crossorigin="anonymous"></script>
       </head>
 
       <body className={inter.className}>
         <Navbar />
         {children}
         <Footer />
+
+        {/* Razorpay Script */}
+        <Script src="https://checkout.razorpay.com/v1/checkout.js" />
       </body>
-      <Script src="https://checkout.razorpay.com/v1/checkout.js" />
     </html>
   );
 }
