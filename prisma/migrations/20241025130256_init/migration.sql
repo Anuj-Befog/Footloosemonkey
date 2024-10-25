0c092d19845e6c65e750b2662bffd595ad07c2a5
Anuj3553 <anujverma3553@gmail.com>
2024-10-25 18:37:17 +0530

    Prisma Setup with Neon DB Completed

---
+++ prisma/migrations/20241025130256_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "Submission" (
+    "id" TEXT NOT NULL,
+    "publicId" TEXT NOT NULL,
+    "participantId" TEXT NOT NULL,
+    "participantName" TEXT NOT NULL,
+    "participantEmail" TEXT NOT NULL,
+    "participantAge" TEXT NOT NULL,
+    "profilepic" TEXT NOT NULL,
+    "video" TEXT NOT NULL,
+    "participanTalent" TEXT NOT NULL,
+    "postTitle" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "duration" DOUBLE PRECISION NOT NULL,
+    "originalSize" TEXT NOT NULL,
+    "compressedSize" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Submission_publicId_key" ON "Submission"("publicId");
