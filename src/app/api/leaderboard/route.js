710aec315984f789f0c04a51e1018436233932be
Anuj3553 <anujverma3553@gmail.com>
2024-11-06 11:49:26 +0530

    Refactor leaderboard API to use new GET handler and return Response objects

--- src/app/api/leaderboard/route.js
+++ src/app/api/leaderboard/route.js
@@ -1,32 +1,24 @@
-// pages/api/leaderboard.js
-
 import prisma from '../../../lib/prisma'; // Adjust the path as necessary
 
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const leaderboard = await prisma.submission.findMany({
-        orderBy: {
-          voteCount: 'desc', // Sort by vote count descending
-        },
-        take: 10, // Fetch top 10 submissions
-        select: {
-          id: true,
-          participantName: true,
-          participantTalent: true,
-          voteCount: true,
-          createdAt: true,
-        },
-      });
-      console.log(leaderboard);
-      res.status(200).json(leaderboard);
-    } catch (error) {
-      console.error("Error fetching leaderboard:", error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  } else {
-    // Handle any other HTTP method
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+export async function GET(req) {
+  try {
+    const leaderboard = await prisma.submission.findMany({
+      orderBy: {
+        voteCount: 'desc', // Sort by vote count descending
+      },
+      take: 10, // Fetch top 10 submissions
+      select: {
+        id: true,
+        participantName: true,
+        participantTalent: true,
+        voteCount: true,
+        createdAt: true,
+      },
+    });
+    console.log(leaderboard);
+    return new Response(JSON.stringify(leaderboard), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching leaderboard:", error);
+    return new Response(JSON.stringify({ message: 'Internal Server Error' }), { status: 500 });
   }
 }
