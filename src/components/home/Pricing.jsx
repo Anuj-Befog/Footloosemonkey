dfe81a1172358065a0a5365b9e2439de0786d9f9
Anuj3553 <anujverma3553@gmail.com>
2024-11-06 15:12:04 +0530

    Update Pricing component to reflect free pricing for all age groups

--- src/components/home/Pricing.jsx
+++ src/components/home/Pricing.jsx
@@ -11,7 +11,8 @@ const Pricing = () => {
         <div className="bg-white p-8 shadow-xl rounded-lg text-center w-full md:w-1/3">
           <Image src="/home/pricing-toddler.png" alt="Toddler Contestent" width={270} height={180} className="mx-auto mt-3 rounded-lg mb-6" />
           <h3 className="text-3xl font-semibold text-sky-600 mt-7 mb-4">3-5 Years</h3>
-          <p className="text-4xl font-bold text-gray-700 mb-2">₹199</p>
+          <p className="text-4xl font-bold text-gray-700 mb-2 line-through decoration-red-500">₹199</p>
+          <p className="text-4xl font-bold text-sky-700 mb-2">FREE</p>
           <p className="text-lg text-gray-500 mb-6">Plus GST</p>
           <p className="text-md text-gray-600">
             An introduction to performing arts, allowing participants to showcase talent with a short performance and encouragement!
@@ -25,7 +26,8 @@ const Pricing = () => {
         <div className="bg-white p-8 shadow-xl rounded-lg text-center w-full md:w-1/3">
           <Image src="/home/pricing-young.png" alt="Young Contestent" width={300} height={200} className="mx-auto rounded-lg mb-6" />
           <h3 className="text-3xl font-semibold text-sky-600 mb-4">6-8 Years</h3>
-          <p className="text-4xl font-bold text-gray-700 mb-2">₹299</p>
+          <p className="text-4xl font-bold text-gray-700 mb-2 line-through decoration-red-500">₹299</p>
+          <p className="text-4xl font-bold text-sky-700 mb-2">FREE</p>
           <p className="text-lg text-gray-500 mb-6">Plus GST</p>
           <p className="text-md text-gray-600">
             Perfect for young stars starting their journey. Includes stage performance, judge feedback, and fun activities!
@@ -39,7 +41,8 @@ const Pricing = () => {
         <div className="bg-white p-8 shadow-xl rounded-lg text-center w-full md:w-1/3">
           <Image src="/home/pricing-old.png" alt="Older Contestent" width={300} height={200} className="mx-auto rounded-lg mb-6" />
           <h3 className="text-3xl font-semibold text-sky-600 mb-4">9-12 Years</h3>
-          <p className="text-4xl font-bold text-gray-700 mb-2">₹399</p>
+          <p className="text-4xl font-bold text-gray-700 mb-2 line-through decoration-red-500">₹399</p>
+          <p className="text-4xl font-bold text-sky-700 mb-2">FREE</p>
           <p className="text-lg text-gray-500 mb-6">Plus GST</p>
           <p className="text-md text-gray-600">
             Ideal for more experienced performers who want to showcase their talent and compete for bigger rewards.
