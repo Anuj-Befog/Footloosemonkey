1ff055ec5766c99c8c6e8d57ce82e067d4a0e052
Anuj3553 <anujverma3553@gmail.com>
2024-11-07 16:46:39 +0530

    Adjust padding in Collaboration section and fix className in Hero component for improved layout

--- src/components/home/Collaboration.jsx
+++ src/components/home/Collaboration.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Collaboration() {
     return (
-        <section className="collaboration-section bg-[aliceblue] pt-4 pb-1">
+        <section className="collaboration-section bg-[aliceblue] pt-8 pb-1">
             <div className="container mx-auto px-6 text-center">
                 <h2 className="text-3xl font-bold text-blue-500 mb-4">Collaboration with Red Hill School</h2>
                 <p className="text-lg text-gray-700 mb-8">
