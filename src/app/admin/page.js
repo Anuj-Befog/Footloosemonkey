545f4d345b34fe918c391fc6b1157807b7b8402b
Anuj3553 <anujverma3553@gmail.com>
2024-11-08 17:21:53 +0530

    Add admin login functionality with bcryptjs for password hashing; implement form controls and CRUD operations for admin data

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,167 +1,144 @@
-'use client';
+"use client"
 
-import { useState, useEffect } from 'react';
-import { addAdminData, getAdminData } from '../services/index';
-import { toast } from 'react-toastify';
+import React, { useEffect, useState } from 'react'
+import AdminHomeView from '../../components/admin-view/home'
+import { login } from '../services/index'
+import Login from '../../components/admin-view/login'
+import { toast } from 'react-toastify'
 
-export default function AdminPage() {
-    const [selectedTalent, setSelectedTalent] = useState('');
-    const [selectedACharges, setSelectedACharges] = useState('');
-    const [selectedBCharges, setSelectedBCharges] = useState('');
-    const [selectedCCharges, setSelectedCCharges] = useState('');
-    const [dataId, setDataId] = useState(null); // Store fetched _id
-    const [isSubmitting, setIsSubmitting] = useState(false);
+const initialLoginFormData = {
+    username: '',
+    password: '',
+}
 
-    // Load data from getAdminData()
+
+const menuItems = [
+    { id: 'home', label: 'Home' },
+]
+
+const AdminView = () => {
+    const [currentSelectedTab, setCurrentSelectedTab] = useState('/')
+
+    const [authUser, setAuthUser] = useState(false)
+    const [loginFormData, setLoginFormData] = useState(initialLoginFormData)
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
     useEffect(() => {
-        const fetchData = async () => {
-            const response = await getAdminData();
-            if (response.success && response.data && response.data.length > 0) {
-                setDataId(response.data[0]._id); // Assuming the response contains data with _id
-            } else {
-                console.error('Error fetching data:', response.message);
+        setAuthUser(JSON.parse(sessionStorage.getItem('authUser')))
+    }, [])
+
+    async function handleLogin() {
+        if (!loginFormData.username || !loginFormData.password) {
+            toast.error('Please fill in all fields')
+            return;
+        }
+        try {
+            const res = await login(loginFormData)
+            if (res?.success) {
+                setAuthUser(true)
+                toast.success(res?.message || 'Login successful')
+                sessionStorage.setItem('authUser', JSON.stringify(true))
             }
-        };
-
-        fetchData();
-    }, []);
-
-    const handleTalentChange = (e) => {
-        const value = e.target.value;
-        setSelectedTalent(value);
-        // Reset selected charges when talent changes
-        setSelectedACharges('');
-        setSelectedBCharges('');
-        setSelectedCCharges('');
-    };
-
-    const handleSubmit = async () => {
-        // Form validation
-        if (!selectedTalent || !selectedACharges || !selectedBCharges || !selectedCCharges) {
-            toast.error("Please fill in all fields.");
-            return;
+            else {
+                toast.error(res?.message || 'Login failed')
+            }
+        } catch (error) {
+            toast.error(res?.message || 'Login failed')
         }
+    }
 
-        if (!dataId) {
-            console.log('Data ID not found. Cannot submit form.');
-            return;
+    function handleLogout() {
+        const confirmLogout = window.confirm('Are you sure you want to logout?');
+        if (confirmLogout) {
+            setAuthUser(false);
+            sessionStorage.removeItem('authUser');
         }
+        toast.success('Logout successful');
+    }
 
-        // Disable the button during form submission
-        setIsSubmitting(true);
-
-        // Prepare form data to be sent to the server
-        const formData = {
-            _id: dataId,  // Pass the fetched _id
-            selectedTalent,
-            selectedACharges,
-            selectedBCharges,
-            selectedCCharges,
-        };
-
-        console.log('Form Data:', formData);
-
-        // Call the addAdminData function to post the data
-        const response = await addAdminData(formData);
-
-        // Check the response and navigate or display a message accordingly
-        if (response.success) {
-            toast.success("Form Saved");
-            // Optionally, reset the form after successful submission
-            setSelectedTalent('');
-            setSelectedACharges('');
-            setSelectedBCharges('');
-            setSelectedCCharges('');
-        } else {
-            // Handle the error accordingly (e.g., show an error message)
-            toast.error(`Error: ${response.message}`);
-        }
-
-        // Re-enable the button after submission process is complete
-        setIsSubmitting(false);
-    };
+    if (!authUser) return <Login formData={loginFormData} handleLogin={handleLogin} setFormData={setLoginFormData} />
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-3xl font-bold mb-6 text-blue-600">Welcome to the Admin Panel</h1>
-            <div className="bg-white rounded-lg shadow-lg p-6 w-80">
-                {/* Dropdown for Talent Categories */}
-                <select
-                    value={selectedTalent}
-                    onChange={handleTalentChange}
-                    className="border border-gray-300 rounded-lg p-2 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
-                    <option value="">Select a category</option>
-                    <option value="Acting">Acting</option>
-                    <option value="Dancing">Dancing</option>
-                    <option value="Mimicry">Mimicry</option>
-                    <option value="Singing">Singing</option>
-                </select>
+        <>
+            {/*  Navbar */}
+            <div className='border-b-2 border-gray-300'>
+                <nav className='flex items-center justify-between p-4 bg-gray-100'>
+                    <div className='flex items-center'>
+                        <h1 className='text-2xl font-bold text-black'>Admin Panel</h1>
+                    </div>
 
-                {/* Dropdown for Group A Charges based on selected Talent */}
-                {selectedTalent && (
-                    <select
-                        value={selectedACharges}
-                        onChange={(e) => setSelectedACharges(e.target.value)}
-                        className="border border-gray-300 rounded-lg p-2 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >
-                        <option value="">Select Group A Charges</option>
-                        <option value={0}>₹ 0</option>
-                        <option value={9}>₹ 9</option>
-                        <option value={49}>₹ 49</option>
-                        <option value={99}>₹ 99</option>
-                        <option value={199}>₹ 199</option>
-                    </select>
-                )}
+                    <div className='hidden items-center md:flex space-x-6'>
+                        {menuItems.map((item, index) => (
+                            <button
+                                key={index}
+                                type='button'
+                                onClick={() => setCurrentSelectedTab(item.id)}
+                                className='p-4 font-bold text-xl text-black hover:text-blue-500'
+                            >
+                                {item.label}
+                            </button>
+                        ))}
 
-                {/* Dropdown for Group B Charges based on selected Talent */}
-                {selectedTalent && (
-                    <select
-                        value={selectedBCharges}
-                        onChange={(e) => setSelectedBCharges(e.target.value)} // Corrected state update
-                        className="border border-gray-300 rounded-lg p-2 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >
-                        <option value="">Select Group B Charges</option>
-                        <option value={0}>₹ 0</option>
-                        <option value={9}>₹ 9</option>
-                        <option value={49}>₹ 49</option>
-                        <option value={99}>₹ 99</option>
-                        <option value={199}>₹ 149</option>
-                        <option value={199}>₹ 199</option>
-                        <option value={249}>₹ 249</option>
-                        <option value={299}>₹ 299</option>
-                    </select>
-                )}
+                        {/* Logout */}
+                        <button
+                            onClick={handleLogout}
+                            class="group flex items-center justify-start w-11 h-11 bg-red-600 rounded-full cursor-pointer relative overflow-hidden transition-all duration-200 shadow-lg hover:w-32 hover:rounded-lg active:translate-x-1 active:translate-y-1"
+                        >
+                            <div
+                                class="flex rounded-full items-center justify-center w-full transition-all duration-300 group-hover:justify-start group-hover:px-3"
+                            >
+                                <svg class="w-4 h-4" viewBox="0 0 512 512" fill="white">
+                                    <path
+                                        d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"
+                                    ></path>
+                                </svg>
+                            </div>
+                            <div
+                                class="absolute rounded-full right-5 transform translate-x-full opacity-0 text-white text-lg font-semibold transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100"
+                            >
+                                Logout
+                            </div>
+                        </button>
 
-                {/* Dropdown for Group C Charges based on selected Talent */}
-                {selectedTalent && (
-                    <select
-                        value={selectedCCharges}
-                        onChange={(e) => setSelectedCCharges(e.target.value)} // Corrected state update
-                        className="border border-gray-300 rounded-lg p-2 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    >
-                        <option value="">Select Group C Charges</option>
-                        <option value={0}>₹ 0</option>
-                        <option value={9}>₹ 9</option>
-                        <option value={49}>₹ 49</option>
-                        <option value={99}>₹ 99</option>
-                        <option value={199}>₹ 149</option>
-                        <option value={199}>₹ 199</option>
-                        <option value={249}>₹ 249</option>
-                        <option value={299}>₹ 299</option>
-                        <option value={349}>₹ 349</option>
-                        <option value={399}>₹ 399</option>
-                    </select>
-                )}
+                        {/* Menu Bar */}
+                        <button className='hidden text-xl font-bold text-black md:hidden' onClick={() => setIsNavbarOpen(!isNavbarOpen)}>
+                            ☰
+                        </button>
+                    </div>
 
-                <button
-                    disabled={isSubmitting}
-                    onClick={handleSubmit}
-                    className={`w-full bg-blue-500 text-white font-semibold py-2 rounded-lg shadow ${isSubmitting ? 'opacity-50 cursor-not-allowed' : ' hover:bg-blue-600 cursor-pointer'} transition duration-200`}
-                >
-                    Submit
-                </button>
+                    {/* Mobile Menu */}
+                    <div className={`fixed inset-0 bg-gray-900 bg-opacity-75 z-40 transition-opacity duration-300 ${isNavbarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'} md:hidden`}>
+                        <div className='fixed inset-0 bg-red-500 flex justify-center items-center flex-col space-y-4'>
+                            <button className='text-4xl font-bold text-white absolute top-[20px] left-[30px]' onClick={() => setIsNavbarOpen(false)}>X</button>
+                            {menuItems.map((item, index) => (
+                                <button
+                                    key={index}
+                                    type='button'
+                                    onClick={() => { setCurrentSelectedTab(item.id); setIsNavbarOpen(false); }}
+                                    className='block w-full p-4 font-bold text-3xl text-white'
+                                >
+                                    {item.label}
+                                </button>
+                            ))}
+                            <button
+                                onClick={() => { setAuthUser(false); sessionStorage.removeItem('authUser'); setIsNavbarOpen(false); }}
+                                className='block w-full p-4 font-bold text-3xl text-white'
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    </div>
+                </nav>
             </div>
-        </div>
-    );
+            {/* Content */}
+            <div className='bg-gray-100'>
+                <h1 className="text-4xl pt-4 text-center font-extrabold text-gray-800 md:text-5xl lg:text-6xl mb-4">
+                    Customize Contest
+                </h1>
+                <AdminHomeView />
+            </div>
+        </>
+    )
 }
+
+export default AdminView
--- src/app/services/index.js
+++ src/app/services/index.js
@@ -43,6 +43,101 @@ export async function getAdminData() {
     }
 }
 
+// CRUD Operation for get, add, update and delete for Admin
+
+export async function login(formData) {
+    try {
+        const response = await fetch(`/api/admin`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.error(`Error logging in: ${errorData.message || 'Unknown error'}`);
+            return { success: false, message: errorData.message || 'Unknown error' };
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (e) {
+        console.error(`Error in login: ${e.message}`);
+        return { success: false, message: e.message };
+    }
+}
+
+export async function addData(currentTab, formData) {
+    try {
+        const response = await fetch(`/api/${currentTab}/add`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.error(`Error adding data: ${errorData.message || 'Unknown error'}`);
+            return { success: false, message: errorData.message || 'Unknown error' };
+        }
+
+        const result = await response.json();
+        console.log(result)
+        return result;
+    } catch (e) {
+        console.error(`Error in addData: ${e.message}`);
+        return { success: false, message: e.message };
+    }
+}
+
+export async function getData(currentTab) {
+    try {
+        const response = await fetch(`/api/${currentTab}/get`, {
+            method: 'GET'
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.error(`Error getting data: ${errorData.message || 'Unknown error'}`);
+            return { success: false, message: errorData.message || 'Unknown error' };
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (e) {
+        console.error(`Error in getData: ${e.message}`);
+        return { success: false, message: e.message };
+    }
+}
+
+export async function updateData(currentTab, formData) {
+    try {
+        const response = await fetch(`/api/${currentTab}/update`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.error(`Error updating data: ${errorData.message || 'Unknown error'}`);
+            return { success: false, message: errorData.message || 'Unknown error' };
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (e) {
+        console.error(`Error in updateData: ${e.message}`);
+        return { success: false, message: e.message };
+    }
+}
+
 // Service for Registration
 export async function addRegistrationData(formData) {
     try {
