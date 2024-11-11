52baf0e6f368edea10902829d3b72c4d77d4e38b
Anuj3553 <anujverma3553@gmail.com>
2024-11-11 13:03:54 +0530

    Wrap AdBanner component in a div for improved layout and spacing

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -25,14 +25,16 @@ const AdBanner: React.FC<AdBannerTypes> = ({ dataAdSlot, dataAdFormat, dataFullW
     }, []);
 
     return (
-        <ins
-            className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-8590034113156778"
-            data-ad-slot={dataAdSlot}
-            data-ad-format={dataAdFormat}
-            data-full-width-responsive={dataFullWidthResponsive.toString()}
-        />
+        <div style={{overflow: 'hidden', margin: '5px'}}>
+            <ins
+                className="adsbygoogle"
+                style={{ display: "block" }}
+                data-ad-client="ca-pub-8590034113156778"
+                data-ad-slot={dataAdSlot}
+                data-ad-format={dataAdFormat}
+                data-full-width-responsive={dataFullWidthResponsive.toString()}
+            />
+        </div>
     );
 };
 
